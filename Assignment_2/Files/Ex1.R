f2096539abf973836b6e1a901cfd706d1fcf7a12
Leander De Souter <leander.desouter@ugent.be>
2022-02-25 17:36:15 +0100

    Add update ex1

--- Assignment_2/Files/Ex1.R
+++ Assignment_2/Files/Ex1.R
@@ -51,6 +51,13 @@ r_squared <- function(true, predictions) {
 r_squared(test$Sales, pred)
 
 ###Q3:
+#split training set in validation + training set
+valind <- trainind[(round(length(trainind) * (0.5)) + 1):length(trainind)]
+trainind <- trainind[1:round(length(trainind) * 0.5)]
+
+# actual subsetting
+train <- Advertising[trainind, ]
+val <- Advertising[valind, ]
 ###a)
 m0 <- lm(train$Sales~1, data = train)
 m1tv <- lm(train$Sales~train$TV, data = train)
@@ -70,10 +77,61 @@ summary(m2newspaperradio)
 summary(m2tvnewspaper)
 summary(m2tvradio)
 summary(m3)
+
 #best models per group: m0, m1tv, m2tvradio, m3
 
+#make predictions
+predm0 <- predict(m0, newdata = val)
+predm1tv <- predict(m1tv, newdata = val)
+predm2tvradio <- predict(m2tvradio, newdata = val)
+predm3 <- predict(m3, newdata = val)
+
+install.packages('nlme')
+p_load(nlme)
+logLik(m0)
 ###b)
 
+adj_r_squared <- function(model) {
+  return(summary(model)$adj_r_squared)
+  }
+BIC <- function(model, nparameters){ 
+  return(-2 * logLik(model) + log(50) * nparameters)
+  }
+AIC <- function(model, nparameters){
+  return (-2/50 * logLik(model) + 2 * nparameters/50)
+}
+CVSS <-function(true, predictions){
+  return((val$Sales-predictions)/(1-0.001))
+}
+# all in one vector
+performance_estimate <- function(true, predictions,model,nparameters) {
+  return(c(CVSS(true, predictions), AIC(model, nparameters),
+           BIC(model,nparameters), adj_r_squared(model)))
+}
+
+performance_estimate(val$Sales,predm0,m0,1)
+performance_estimate(val$Sales,predm1tv,m1tv,2)
+performance_estimate(val$Sales,predm2tvradio,m2tvradio,3)
+performance_estimate(val$Sales,predm3,m3,4)
+summary(m1tv$adj_r_squared)
+
+#calculate BIC of models
+AIC(m0,1)
+BIC(m1tv)
+BIC(m2tvradio)
+BIC(m3)
+
+install.packages('AICcmodavg')
+library(AICcmodavg)
+
+#define list of models
+models <- list(m0,m1tv, m2tvradio, m3)
+
+#specify model names
+mod.names <- c('null model', 'm1tv', 'm2tvradio', 'm3')
+
+#calculate AIC of each model
+aictab(cand.set = models, modnames = mod.names)
 
 
 
